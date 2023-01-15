cfb639b5da170a07089b692d4b186e8834dcc014
kibethh <kibethh@gmail.com>
2023-01-15 09:48:32 +0300

    implemented setupApp function for middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { setupApp } from './setup-app';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['dhsadhsaghda'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  setupApp(app);
   await app.listen(3000);
 }
 bootstrap();
